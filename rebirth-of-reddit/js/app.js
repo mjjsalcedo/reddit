d70bb0a77538ca9a39b17bcc50bb67f5456ecc0d
Oksana Harris <oksanaharris@Oksanas-MacBook-Pro.local>
2017-07-08 20:01:30 -1000

    structure for posts and populating most but without images

--- rebirth-of-reddit/js/app.js
+++ rebirth-of-reddit/js/app.js
@@ -1,4 +1,5 @@
 
+const contentDiv = document.getElementById('content');
 
 function createXHR(method, url, cb){
   var oReq = new XMLHttpRequest();
@@ -8,13 +9,35 @@ function createXHR(method, url, cb){
   return oReq;
 }
 
-function populatePage(){
-  var data = JSON.parse(this.responseText);
-  console.log("me!", data.data.children[0].data.title);
-  var postsArray = data.data.children;
-  }
-
 var request = createXHR("GET", "https://www.reddit.com/r/lol.json", populatePage);
 
+function populatePage(){
+  var allData = JSON.parse(this.responseText);
+  console.log("me!", allData.data.children[0].data.title);
+  var postsArray = allData.data.children;
+
+  postsArray.forEach( (post) => {
+    var postDivEl = document.createElement('div');
+    contentDiv.appendChild(postDivEl);
+
+    var imageEl = document.createElement('img');
+    if (!post.data.url){
+      imageEl.src = post.data.media.oembed.thumbnail_url;
+    } else {
+      imageEl.src = post.data.url;
+    }
+
+    console.log('image source' , imageEl.src);
+    postDivEl.appendChild(imageEl);
+
+    var titleEl = document.createElement('h2');
+    titleEl.innerHTML = post.data.title;
+    postDivEl.appendChild(titleEl);
+
+    var detailsEl = document.createElement('p');
+    detailsEl.innerHTML = 'by ' + post.data.author + ' * ' + post.data.created + ' * upcounts: ' + post.data.ups;
+    postDivEl.appendChild(detailsEl);
+  });
+}
 
 
